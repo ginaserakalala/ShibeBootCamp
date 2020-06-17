48d6bbe8c950c810a4a34c0789191e7c46ad3c96
Shibe Serakalala <sserakal@c4r4s1.wethinkcode.co.za>
2020-06-17 15:39:09 +0200

    Printing alphabets from a-z

--- d02/ex00/ft_print_alphabet.c
+++ d02/ex00/ft_print_alphabet.c
@@ -1,12 +1,40 @@
-void ft_putchar(char c);
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sserakal <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/17 14:19:07 by sserakal          #+#    #+#             */
+/*   Updated: 2020/06/17 15:37:34 by sserakal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+ 	write(1,&c,1);
+}
+
 void ft_print_alphabet(void)
 {
 	char letter;
 	letter  = 'a';
 	while(letter <= 'z')
 	{
-		letter++;
-		ft_putchar(letter);
+			ft_putchar(letter);
+			letter++;
 	}
-	return 0;
-}
+
+
+	}
+
+
+
+
+
+
+
+
+
+			
