c5dd4cbb59bbbc438bc0134f92ba143f3ab7a7d1
Shibe Serakalala <sserakal@c4r4s1.wethinkcode.co.za>
2020-06-17 16:30:04 +0200

    print reverse alphabet

--- d02/ex01/ft_print_reverse.c
+++ d02/ex01/ft_print_reverse.c
@@ -6,28 +6,24 @@
 /*   By: sserakal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/17 15:48:17 by sserakal          #+#    #+#             */
-/*   Updated: 2020/06/17 16:10:36 by sserakal         ###   ########.fr       */
+/*   Updated: 2020/06/17 16:29:15 by sserakal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 	#include <unistd.h>	
 
     void ft_putchar(char c)
 	{
-		write(1,&c,26);
+		write(1,&c,1);
 	}
     void ft_print_reverse_alphabet(void)
 	{
 		char letter;
 		letter = 'z';
-		while(letter > 'a')
+		while(letter >= 'a')
 		{
 			ft_putchar(letter);
-			letter++;
+			letter--;
 		}
 
 	}
- 	int main()
-	{
-		ft_print_reverse_alphabet();
-		return 0;
-    } 
+ 	
