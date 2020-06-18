32d65206ebc2bfa64fc0e249b6a25b7229efc765
Gina <shibe.serakalala@gmail.com>
2020-06-18 19:50:49 +0200

    combo of 2

--- d02/ex05/ft_print_comb2.c
+++ d02/ex05/ft_print_comb2.c
@@ -1,2 +1,18 @@
 void ft_putchar(char c);
-void ft_print_comb2(void);
+void ft_print_comb2(void)
+{
+	unsigned i,j;
+	i = 0;
+	while(i < 9)
+	{
+		j = 0;
+		while(j < 9)
+		{
+			if(i != j && j!= i)
+			{
+				ft_putchar(i);
+				j++;
+				i++;
+			}
+
+		}
