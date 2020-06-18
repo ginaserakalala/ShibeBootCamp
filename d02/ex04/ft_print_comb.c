28cd59e0d20b99415ee0fbc79c2733319d191a46
Gina <shibe.serakalala@gmail.com>
2020-06-18 19:41:07 +0200

    updated my work on c

--- d02/ex04/ft_print_comb.c
+++ d02/ex04/ft_print_comb.c
@@ -1,5 +1,24 @@
 void ft_putchar(char c);
 void ft_print_comb(void)
 {
+	unsigned int i,j,h;
+	i = 0;
+	while(i < 9)
+	{
+		j = 0;
+		while(j < 3)
+		{
+			j++;
+			h = 0;
+			while(h < 9)
+			{
+				if(if i != k && j != k && k != i)
+				{
+					ft_putchar(i);;
+				}
 
+			}
+
+		}
+	}
 }
