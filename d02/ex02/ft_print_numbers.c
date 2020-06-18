f7c8d63d3372dc9dfe396775734e741b46a7878c
Shibe Serakalala <sserakal@c4r4s1.wethinkcode.co.za>
2020-06-18 10:27:54 +0200

    added work

--- d02/ex02/ft_print_numbers.c
+++ d02/ex02/ft_print_numbers.c
@@ -6,7 +6,7 @@
 /*   By: sserakal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/18 08:41:09 by sserakal          #+#    #+#             */
-/*   Updated: 2020/06/18 08:56:09 by sserakal         ###   ########.fr       */
+/*   Updated: 2020/06/18 10:27:22 by sserakal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -14,12 +14,12 @@
 void ft_putchar(char c);
 void ft_print_numbers(void)
 {
-char numbers;
-numbers = '0';
-while(numbers <= '9')
-{
-ft_putchar(numbers);
-numbers++;
-}
+	char numbers;
+	numbers = '0';
+	while(numbers <= '9')
+	{
+		ft_putchar(numbers);
+		numbers++;
+	}
 
 }
