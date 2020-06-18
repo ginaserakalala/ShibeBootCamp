e5ef31b37f8c4257c0d9e43c68dd657d57e9a4b0
Shibe Serakalala <sserakal@c4r4s1.wethinkcode.co.za>
2020-06-18 10:39:30 +0200

    changes made to ex00

--- d02/ex00/ft_print_alphabet.c
+++ d02/ex00/ft_print_alphabet.c
@@ -6,7 +6,7 @@
 /*   By: sserakal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/17 14:19:07 by sserakal          #+#    #+#             */
-/*   Updated: 2020/06/18 08:58:41 by sserakal         ###   ########.fr       */
+/*   Updated: 2020/06/18 10:38:06 by sserakal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -18,8 +18,8 @@ void ft_print_alphabet(void)
 	letter  = 'a';
 	while(letter <= 'z')
 	{
-			ft_putchar(letter);
-			letter++;
+		ft_putchar(letter);
+		letter++;
 	}
 
 
@@ -33,4 +33,4 @@ void ft_print_alphabet(void)
 
 
 
-			
+
