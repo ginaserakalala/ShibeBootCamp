8721f107595007a36a2095e90f28072b4cc66454
Shibe Serakalala <sserakal@c4r4s1.wethinkcode.co.za>
2020-06-19 15:43:32 +0200

    combo 2

--- d02/ex05/ft_print_comb2.c
+++ d02/ex05/ft_print_comb2.c
@@ -1,18 +1,44 @@
-void ft_putchar(char c);
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sserakal <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/19 14:46:19 by sserakal          #+#    #+#             */
+/*   Updated: 2020/06/19 15:40:53 by sserakal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+void ft_putchar(char c)
+{
+	write(1,&c,1);
+}
 void ft_print_comb2(void)
 {
-	unsigned i,j;
-	i = 0;
-	while(i < 9)
-	{
-		j = 0;
-		while(j < 9)
-		{
-			if(i != j && j!= i)
-			{
-				ft_putchar(i);
-				j++;
-				i++;
-			}
+	char a;
+	char b;
+	char c;
 
+
+	while(++a <= 99)
+	{
+		while(++b <= 99)
+		{
+			ft_putchar(a);
+			ft_putchar(b);
+
+			if(a < '8'|| b < '9')
+			{
+				ft_putchar(' ');
+				ft_putchar(',');
+
+			}
 		}
+	}
+}
+int main()
+{
+	ft_print_comb2();
+	return (0);
+}
