41ff24992880613f309a30f2e2e2ded83f8ba68e
Shibe Serakalala <sserakal@c4r4s1.wethinkcode.co.za>
2020-06-20 10:55:44 +0200

    42

--- d02/ex06/ft_putnbr.c
+++ d02/ex06/ft_putnbr.c
@@ -1,20 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sserakal <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/20 08:57:30 by sserakal          #+#    #+#             */
+/*   Updated: 2020/06/20 10:55:07 by sserakal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
 void ft_putchar(char c)
 {
 	write(1,&c,1);
 }
+
 void ft_putnbr(int nb)
 {
-	char num;
-	num = nb + '0';
-	ft_putchar(num);
+
+	if(nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		ft_putnbr(147483648);
+	}
+	else if(nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(-nb);
+	}
+	else if(nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);	 
+	}
+	else
+	{
+		ft_putchar(nb + '0');
+	}
 }
 int main()
 {
-	int num = 42;
-	char r;
-	r = num + '0';
-	ft_putnbr(r);
+	ft_putnbr(42);
 	return(0);
 }
