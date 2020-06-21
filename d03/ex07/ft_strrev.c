faf700d0bd8e52d4a6504eb1e87f495cc19a8555
Gina <shibe.serakalala@gmail.com>
2020-06-21 21:36:57 +0200

    attempt at strrev

--- d03/ex07/ft_strrev.c
+++ d03/ex07/ft_strrev.c
@@ -1,1 +1,21 @@
+#include <unistd.h>
+char *ft_strrev(char *str)
+{
+	int rev;
+	rev = 0;
+	int len;
+	len = str[rev];
+	while(str[rev] != '\0')
+	{
+		str[rev] = len -1;
+		--rev;
+	}
+	write(1,&rev,1);
+	return (&str);
+}
+int main()
+{
+	ft_strrev("loop");
+	return (0);
+}
 
