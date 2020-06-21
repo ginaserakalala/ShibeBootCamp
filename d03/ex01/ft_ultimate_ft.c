e21c41a8e3d49ac17226f147f56e348321d1442a
Shibe Serakalala <sserakal@c4r4s1.wethinkcode.co.za>
2020-06-21 13:49:31 +0200

    ex02 done

--- d03/ex01/ft_ultimate_ft.c
+++ d03/ex01/ft_ultimate_ft.c
@@ -6,40 +6,13 @@
 /*   By: sserakal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/21 11:42:18 by sserakal          #+#    #+#             */
-/*   Updated: 2020/06/21 13:47:20 by sserakal         ###   ########.fr       */
+/*   Updated: 2020/06/21 13:49:22 by sserakal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <unistd.h>
 
 void ft_ultimate_ft(int *********nbr)
 {   
 	*********nbr= 42;
 	write(1,"42",2);
 }
-int main()
-{
-	int a;
-	int *ptr;
-	int **ptr2;
-	int ***ptr3;
-	int ****ptr4;
-	int *****ptr5;
-	int ******ptr6;
-	int *******ptr7;
-	int ********ptr8;
-	int *********ptr9;
-
-	a = 7;
-	ptr = &a;
-	ptr2 = &ptr;
-	ptr3 = &ptr2;
-	ptr4 = &ptr3;
-	ptr5 = &ptr4;
-	ptr6 = &ptr5;
-	ptr7 = &ptr6;
-	ptr8 = &ptr7;
-	ptr9 = &ptr8;
-	ft_ultimate_ft(ptr9);
-}
-
 
