67411e66d2756c9f99c1b3ae4adefe5960e34f3a
Gina <shibe.serakalala@gmail.com>
2020-06-21 22:09:51 +0200

    ex08 updated

--- d03/ex08/ft_atoi.c
+++ d03/ex08/ft_atoi.c
@@ -1,1 +1,21 @@
+#include <unistd.h>
+
+int ft_atoi(char *str)
+{
+	int i;
+	i = 0;
+	int res;
+	res = 0;
+	while(str[i] != '\0')
+	{
+		res = res * 10 + str[i] - '0';
+		i++;
+	}
+	return res;
+}
+int main()
+{
+	ft_atoi("hjjfg");
+	return (0);
+}
 
