a67fb55ba0df03c97609b384422b44122879779a
Gina <shibe.serakalala@gmail.com>
2020-06-21 19:28:26 +0200

    ex03 update

--- d03/ex03/ft_div_mod.c
+++ d03/ex03/ft_div_mod.c
@@ -1,1 +1,14 @@
 
+void	 ft_div_mod(int a, int b, int *div, int *mod)
+{
+	a = 4;
+	b = 2;
+	int res;
+	int res2;
+	*div = a / b;
+	*mod =a % b;
+	res = *div;
+	res2 = *mod;
+	write(1,&res,1);
+	write(1,&res2,1);
+}
