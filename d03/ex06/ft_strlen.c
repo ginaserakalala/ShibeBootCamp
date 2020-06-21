5e9378591d4738ecc1b79b6ddf98c26d7797c9b3
Gina <shibe.serakalala@gmail.com>
2020-06-21 21:03:13 +0200

    updates on ex06

--- d03/ex06/ft_strlen.c
+++ d03/ex06/ft_strlen.c
@@ -1,1 +1,20 @@
+#include <unistd.h>
+int ft_strlen(char *str)
+{
+	int count;
+	count = 0;
+	while(str[count] != '\0')
+	{
+
+		++count;
+	}
+	write(1,&count,1);
+	return (0);
+}
+int main()
+{      
+	char length;
+	ft_strlen(&length);
+	return(0);
+}
 
