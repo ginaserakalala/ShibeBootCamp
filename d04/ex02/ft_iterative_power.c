6ad656786eb6c697aeed9dd3a11c0e85947f8ec1
Shibe Serakalala <sserakal@c4r4s1.wethinkcode.co.za>
2020-06-22 14:21:34 +0200

    ex 02

--- d04/ex02/ft_iterative_power.c
+++ d04/ex02/ft_iterative_power.c
@@ -6,12 +6,11 @@
 /*   By: sserakal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/22 12:19:13 by sserakal          #+#    #+#             */
-/*   Updated: 2020/06/22 14:01:57 by sserakal         ###   ########.fr       */
+/*   Updated: 2020/06/22 14:21:16 by sserakal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdio.h>
+
 int ft_iterative_power(int nb, int power)
 {
 
@@ -27,14 +26,4 @@ int ft_iterative_power(int nb, int power)
 	}
 	return result;
 }
-int main()
-{
-int a = 2;
-int b  = 3;
-int r;
-ft_iterative_power(a,b);
-printf("the value of power applied to a number is: %d\n",r);
-return (0);
-}
-
 
