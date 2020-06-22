3ed5d1a864d80dfb994d2d601350030dff8c2a09
Shibe Serakalala <sserakal@c4r4s1.wethinkcode.co.za>
2020-06-22 09:35:11 +0200

    added ex 03

--- d03/ex03/ft_div_mod.c
+++ d03/ex03/ft_div_mod.c
@@ -1,14 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_div_mod.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sserakal <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/22 09:33:22 by sserakal          #+#    #+#             */
+/*   Updated: 2020/06/22 09:34:44 by sserakal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void	 ft_div_mod(int a, int b, int *div, int *mod)
+void	ft_div_mod(int a, int b, int *div, int *mod)
 {
-	a = 4;
-	b = 2;
-	int res;
-	int res2;
 	*div = a / b;
 	*mod =a % b;
-	res = *div;
-	res2 = *mod;
-	write(1,&res,1);
-	write(1,&res2,1);
 }
