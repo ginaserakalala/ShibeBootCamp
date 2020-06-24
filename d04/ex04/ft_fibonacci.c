790759eefb74527eb4ee7fcf3d589d5579dd20b3
Shibe Serakalala <sserakal@c4r4s1.wethinkcode.co.za>
2020-06-24 14:33:39 +0200

    fixes

--- d04/ex04/ft_fibonacci.c
+++ d04/ex04/ft_fibonacci.c
@@ -1,27 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sserakal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/06/24 10:30:53 by sserakal          #+#    #+#             */
-/*   Updated: 2020/06/24 10:56:21 by sserakal         ###   ########.fr       */
+/*   Created: 2020/06/24 14:32:57 by sserakal          #+#    #+#             */
+/*   Updated: 2020/06/24 14:33:21 by sserakal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <unistd.h>
-#include <stdio.h>
 
-int ft_recursive_power(int nb, int power)
-{
 
-	return 0;
-else
-	return(nb * ft_recursive_power(nb,power - 1));
-
-		}
-int main(void)
-{
-	printf("answer is %d\n:",ft_recursive_power(2,3));
-	return 0;
-}
