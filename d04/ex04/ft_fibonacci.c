ba9b3738482a73569714859ed35d3653cfbf2902
Shibe Serakalala <sserakal@c4r4s1.wethinkcode.co.za>
2020-06-24 16:12:18 +0200

    exercise 04 fibonacci

--- d04/ex04/ft_fibonacci.c
+++ d04/ex04/ft_fibonacci.c
@@ -6,8 +6,20 @@
 /*   By: sserakal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/24 14:32:57 by sserakal          #+#    #+#             */
-/*   Updated: 2020/06/24 14:33:21 by sserakal         ###   ########.fr       */
+/*   Updated: 2020/06/24 16:09:55 by sserakal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+int ft_fibonacci(int index)
+{
+	if(index < 0)
+		return (-1);
+	else if(index == 0)
+		return 0;
+	else if(index == 1)
+		return 1;
+	return(ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
+
 
 
