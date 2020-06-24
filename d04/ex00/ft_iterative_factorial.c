5808c50fb8ba5f1900293138a0fdbaa79c006319
Shibe Serakalala <sserakal@c4r4s1.wethinkcode.co.za>
2020-06-24 08:10:26 +0200

    updated iterated factorial

--- d04/ex00/ft_iterative_factorial.c
+++ d04/ex00/ft_iterative_factorial.c
@@ -6,21 +6,21 @@
 /*   By: sserakal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/22 10:43:28 by sserakal          #+#    #+#             */
-/*   Updated: 2020/06/22 11:51:54 by sserakal         ###   ########.fr       */
+/*   Updated: 2020/06/24 08:09:52 by sserakal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_iterative_factorial(int nb)
+int	ft_iterative_factorial(int nb)
 {
 	int factorial;
+	int loop;
 
 	factorial = 1;
-	while(factorial < nb)
+	loop = 1;
+	if (nb < 0)
+		return 0;
+	while (loop < nb)
 	{
-		factorial++;
-		factorial = factorial * nb;
+		++loop;
+		factorial = factorial * loop;
 	}
-	return factorial;
-
-	return 0;
-}
