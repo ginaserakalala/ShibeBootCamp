87bfaac6aec68a1b36b05dd630e80df53cf32401
Shibe Serakalala <sserakal@c4r4s1.wethinkcode.co.za>
2020-06-25 10:45:53 +0200

    updated

--- d05/ex02/ft_strstr.c
+++ d05/ex02/ft_strstr.c
@@ -6,20 +6,20 @@
 /*   By: sserakal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/25 08:57:19 by sserakal          #+#    #+#             */
-/*   Updated: 2020/06/25 08:59:37 by sserakal         ###   ########.fr       */
+/*   Updated: 2020/06/25 10:44:16 by sserakal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char *ft_strstr(char *str, char *to_find)
 {
 	int i;
-	int p;
+	int position;
 
 	i = 0;
-	while (i < 1)
+	while (str[i] != '\0')
 	{
 		++i;
-		to_find[i] = str[p + i - 1];
+		to_find[i] = str[position + i - 1];
 	}
 	to_find[i] = '\0';
 	return to_find ;
