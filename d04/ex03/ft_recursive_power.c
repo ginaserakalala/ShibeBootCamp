3ded73e44d0d91caf2bcb2491b76e9f49f374c01
Shibe Serakalala <sserakal@c4r4s1.wethinkcode.co.za>
2020-06-25 10:15:26 +0200

    fixed my if else mistakes

--- d04/ex03/ft_recursive_power.c
+++ d04/ex03/ft_recursive_power.c
@@ -6,22 +6,17 @@
 /*   By: sserakal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/24 10:30:53 by sserakal          #+#    #+#             */
-/*   Updated: 2020/06/24 10:56:21 by sserakal         ###   ########.fr       */
+/*   Updated: 2020/06/25 10:14:56 by sserakal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <unistd.h>
-#include <stdio.h>
 
 int ft_recursive_power(int nb, int power)
 {
+	if(nb == 1)
+		return 1;
+	else if(nb == 0)
+		return 0;
+	else
+		return(nb * ft_recursive_power(nb,power - 1));
 
-	return 0;
-else
-	return(nb * ft_recursive_power(nb,power - 1));
-
-		}
-int main(void)
-{
-	printf("answer is %d\n:",ft_recursive_power(2,3));
-	return 0;
 }
